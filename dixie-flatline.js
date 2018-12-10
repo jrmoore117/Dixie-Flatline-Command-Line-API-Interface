f81f6b802459861d18153872618a6853971571db
jrmoore117 <jrmoore117@gmail.com>
2018-12-09 18:40:56 -0800

    Add initial prompt asking user to pick searchh type.

--- dixie-flatline.js
+++ dixie-flatline.js
@@ -18,20 +18,35 @@ var spotify = new Spotify(spotifyImport.spotify.id, spotifyImport.spotify.secret
 console.log("Spotify ID: " + spotify.id + ", Spotify Secret: " + spotify.secret + ".");
 
 // Declare global variables for currentAPI and userQueryTerms
+var currentAPI = "";
+var userQueryTerms = "";
 
 // Console log opening message from Dixie to console
+console.log("========================================")
+console.log("Dixie: Hey, Case. What can I do for ya?");
+console.log("========================================")
+
 // begin a new prompt
-    // type: list
-    // message asking user to pick what to do
-    // choices: spotify, bands in town, omdb
-    // name
+inquirer.prompt([
+    {
+        type: "list",
+        message: "Case: Dixie, I need you to...",
+        choices: ["look up a song.", "find some concert info.", "look up a movie."],
+        name: "searchType"
+    }
 // .then
+]).then(function(response){
     // assign chosen API to currentAPI variable
+    currentAPI = response.searchType;
+
+    console.log(currentAPI);
+
     // new prompt
         // type: list
         // message asking user to pick what to do
         // choices: spotify, bands in town, omdb
         // name
+})
 // .then
     // assign search terms to userQueryTerms variable
     // switch-case statment
