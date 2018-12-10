c8c9d19cf2aaecdec3df7195c6dc0449fd729869
jrmoore117 <jrmoore117@gmail.com>
2018-12-09 23:13:56 -0800

    Refactor code for readability. Use moment.js too format dates in Bands in Town queries.

--- dixie-flatline.js
+++ dixie-flatline.js
@@ -37,8 +37,8 @@ function askDixie() {
             choices: ["look up a song.", "find some concert info.", "look up a movie."],
             name: "searchType"
         }
-        // .then
     ]).then(function (response) {
+        
         // assign chosen API to currentAPI variable
         currentQueryType = response.searchType;
         // console.log(currentQueryType);
@@ -69,8 +69,8 @@ function askDixie() {
                 name: "searchTerms"
             }
 
-            // .then
         ]).then(function (response) {
+            
             // assign search terms to userQueryTerms variable
             userQueryTerms = response.searchTerms;
             // console.log(userQueryTerms);
@@ -82,121 +82,13 @@ function askDixie() {
             // switch-case statment
             switch (currentQueryType) {
                 case "look up a song.":
-                    // query spotify api
-
-                    if(userQueryTerms === ""){
-                        userQueryTerms = "we're finally landing"
-                    }
-
-                    spotify.search({ type: 'track', query: userQueryTerms }, function (err, data) {
-                        if (err) {
-                            return console.log('Error occurred: ' + err);
-                        }
-                        
-                        console.log("Dixie: \"Here's what I found.\"");
-                        console.log("=============================");
-                        console.log("Artist: " + data.tracks.items[0].artists[0].name);
-                        console.log("Album: " + data.tracks.items[0].album.name);
-                        console.log("Song: " + data.tracks.items[0].name);
-                        console.log("Preview: " + data.tracks.items[0].preview_url);
-                        console.log("============================================");
-
-                        restartDixie();
-                    });
-
-                    // console log parsed response
-                    // see above
+                    searchSpotify();
                     break;
                 case "find some concert info.":
-                    
-                    if(userQueryTerms === ""){
-                        userQueryTerms = "tycho"
-                    }
-                
-                    // query bands in town api
-
-                    var options = {
-                        url: "https://rest.bandsintown.com/artists/" + userQueryTerms + "/events?app_id=codingbootcamp",
-                        method: 'GET',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Accept-Charset': 'utf-8',
-                            'User-Agent': 'my-reddit-client'
-                        }
-                    };
-
-                    request(options, function (error, response, body) {
-                        if (error) {
-                            console.log("Dixie: \"Sorry, bud. No luck.\"");
-                            return console.log("=============================");
-                            // return console.log("Error: " + error);
-                        }
-
-                        // console log parsed response
-                        var json = JSON.parse(body);
-
-                        if (json.length < 1) {
-                            console.log("Dixie: \"Sorry, bud. No luck.\"");
-                            console.log("=============================");
-                        } else if (json.errorMessage) {
-                            console.log("Dixie: \"Sorry, bud. No luck.\"");
-                            console.log("=============================");
-                        } else if (json[0].venue.name && json[0].venue.city && json[0].datetime) {
-                            console.log("Dixie: \"Here's what I found.\"");
-                            console.log("=============================");
-                            console.log("Venue: " + json[0].venue.name);
-                            console.log("Location : " + json[0].venue.city);
-                            console.log("Date : " + json[0].datetime);
-                            console.log("============================================");
-                        } else {
-                            console.log("Dixie: \"Sorry, bud. No luck.\"");
-                            console.log("=============================");
-                        }
-
-                        restartDixie();
-                    });
+                    searchBandsInTown();
                     break;
                 case "look up a movie.":
-                    
-                    if(userQueryTerms === ""){
-                        userQueryTerms = "the matrix"
-                    }
-                
-                    // query omdb api
-
-                    var options = {
-                        url: "http://www.omdbapi.com/?apikey=trilogy&t=" + userQueryTerms,
-                        method: 'GET',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Accept-Charset': 'utf-8',
-                            'User-Agent': 'my-reddit-client'
-                        }
-                    };
-
-                    request(options, function (error, response, body) {
-                        if (error) {
-                            console.log("Dixie: \"Sorry, bud. No luck.\"");
-                            return console.log("=============================");
-                            // return console.log("Error: " + error);
-                        }
-
-                        var json = JSON.parse(body);
-
-                        console.log("Dixie: \"Here's what I found.\"");
-                        console.log("=============================");
-                        console.log("Title: " + json.Title);
-                        console.log("Release Year: " + json.Year);
-                        console.log("IMDB Rating: " + json.Ratings[0].Value);
-                        console.log("Rotten Tomatoes Rating: " + json.Ratings[1].Value);
-                        console.log("Country: " + json.Country);
-                        console.log("Language: " + json.Language);
-                        console.log("Plot: " + json.Plot);
-                        console.log("Actors: " + json.Actors);
-                        console.log("============================================");
-
-                        restartDixie();
-                    });
+                    searchOMDB();
                     break;
             }
         })
@@ -226,6 +118,122 @@ function restartDixie() {
             return console.log("=====================================");
         }
     })
+}
+
+function searchSpotify() {
+    // query spotify api
+    if (userQueryTerms === "") {
+        userQueryTerms = "we're finally landing"
+    }
+
+    spotify.search({ type: 'track', query: userQueryTerms }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log("Dixie: \"Here's what I found.\"");
+        console.log("=============================");
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("Song: " + data.tracks.items[0].name);
+        console.log("Preview: " + data.tracks.items[0].preview_url);
+        console.log("============================================");
+
+        restartDixie();
+    });
+}
+
+function searchBandsInTown() {
+    if (userQueryTerms === "") {
+        userQueryTerms = "tycho"
+    }
+
+    // query bands in town api
+
+    var options = {
+        url: "https://rest.bandsintown.com/artists/" + userQueryTerms + "/events?app_id=codingbootcamp",
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
+            'User-Agent': 'my-reddit-client'
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log("Dixie: \"Sorry, bud. No luck.\"");
+            return console.log("=============================");
+            // return console.log("Error: " + error);
+        }
+
+        // console log parsed response
+        var json = JSON.parse(body);
+
+        var reformatDate = moment(json[0].datetime).format("MM/DD/YYYY")
+
+        if (json.length < 1) {
+            console.log("Dixie: \"Sorry, bud. No luck.\"");
+            console.log("=============================");
+        } else if (json.errorMessage) {
+            console.log("Dixie: \"Sorry, bud. No luck.\"");
+            console.log("=============================");
+        } else if (json[0].venue.name && json[0].venue.city && json[0].datetime) {
+            console.log("Dixie: \"Here's what I found.\"");
+            console.log("=============================");
+            console.log("Venue: " + json[0].venue.name);
+            console.log("Location : " + json[0].venue.city);
+            console.log("Date : " + reformatDate);
+            console.log("============================================");
+        } else {
+            console.log("Dixie: \"Sorry, bud. No luck.\"");
+            console.log("=============================");
+        }
+
+        restartDixie();
+    });
+}
+
+function searchOMDB() {
+    if (userQueryTerms === "") {
+        userQueryTerms = "the matrix"
+    }
+
+    // query omdb api
+
+    var options = {
+        url: "http://www.omdbapi.com/?apikey=trilogy&t=" + userQueryTerms,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
+            'User-Agent': 'my-reddit-client'
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log("Dixie: \"Sorry, bud. No luck.\"");
+            return console.log("=============================");
+            // return console.log("Error: " + error);
+        }
+
+        var json = JSON.parse(body);
+
+        console.log("Dixie: \"Here's what I found.\"");
+        console.log("=============================");
+        console.log("Title: " + json.Title);
+        console.log("Release Year: " + json.Year);
+        console.log("IMDB Rating: " + json.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + json.Ratings[1].Value);
+        console.log("Country: " + json.Country);
+        console.log("Language: " + json.Language);
+        console.log("Plot: " + json.Plot);
+        console.log("Actors: " + json.Actors);
+        console.log("============================================");
+
+        restartDixie();
+    });
 }
 
 askDixie();
