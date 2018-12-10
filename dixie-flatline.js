63a5a976e8676b6cda6dfd8377c669d9ec4894d5
jrmoore117 <jrmoore117@gmail.com>
2018-12-09 18:30:25 -0800

    Add pseudo code to dixie-flatline.js.

--- dixie-flatline.js
+++ dixie-flatline.js
@@ -1,9 +1,12 @@
+// require node modules for api requests
+
 // Require .env file
 require("dotenv").config();
 
-// Require inquirer module
+// Require inquirer module for terminal user interface
 var inquirer = require("inquirer");
 
+// Import spotify API key
 var spotifyImport = require("./keys.js");
 
 function Spotify(id, secret) {
@@ -12,6 +15,44 @@ function Spotify(id, secret) {
 }
 
 var spotify = new Spotify(spotifyImport.spotify.id, spotifyImport.spotify.secret);
-
 console.log("Spotify ID: " + spotify.id + ", Spotify Secret: " + spotify.secret + ".");
 
+// Declare global variables for currentAPI and userQueryTerms
+
+// Console log opening message from Dixie to console
+// begin a new prompt
+    // type: list
+    // message asking user to pick what to do
+    // choices: spotify, bands in town, omdb
+    // name
+// .then
+    // assign chosen API to currentAPI variable
+    // new prompt
+        // type: list
+        // message asking user to pick what to do
+        // choices: spotify, bands in town, omdb
+        // name
+// .then
+    // assign search terms to userQueryTerms variable
+    // switch-case statment
+        // case: spotify
+            // query spotify api
+            // console log parsed response
+                // .then
+                    // new prompt
+                        // type: confirm
+                        // message asking user if they want another search
+                        // name
+                    // .then
+                        // if confirm = true
+                            // call dixie flatline function again
+                        // else
+                            // end program
+        // case: bands in town
+            // query bands in town api
+            // console log parsed response
+                // see lines 41-50
+        // case: omdb
+            // query omdb api
+            // console log parsed response
+                // see lines 41-50
